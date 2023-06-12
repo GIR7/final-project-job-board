304b3f4b36ea07a94da1c7d611b0fc84298a9790
Yihui <0218tyh@gmail.com>
2023-06-11 20:00:10 -0400

    Update frontend route to get user detail and post it to backend

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,10 @@
-import React, {useEffect, useState} from "react";
+import axios from "axios";
+import React, {useContext, useEffect, useState} from "react";
 import {useMsal} from "@azure/msal-react";
 import {useIsAuthenticated} from "@azure/msal-react";
-
-
+import { UserContext } from '../Services/UserIdContext.tsx'
 const NavBar = () => {
-	
 	const isAuth = useIsAuthenticated();
-	
 	return (
 		<div >
 			{isAuth ? <SignOutButton /> : <SignInButton />}
@@ -15,19 +13,52 @@ const NavBar = () => {
 };
 
 const SignInButton = () => {
-	const {instance} = useMsal();
-	const handleSignin=()=>{
-		instance.loginRedirect({
-			scopes:['user.read']
-		})
+	
+	const { instance, accounts } = useMsal();
+	const { updateUserId } = useContext(UserContext);
+	
+	const handleSignIn = async () => {
+		try {
+			const response = await instance.loginPopup({
+				scopes: ["user.read"],
+			});
+			
+			// Get the user's account
+			// Get the user's details using Microsoft Graph API
+			// Send the user's details to the backend
+			const userDetails = await getUserandCreate();
+
+		} catch (error) {
+			console.log("Sign-in error: ", error);
+		}
+	};
+	
+	const getUserandCreate = async () => {
+		try {
+			// Call Microsoft Graph API to get the user's details
+			const userDetails = await instance.getActiveAccount()
+			const username =  userDetails.name
+			const useremail = userDetails.username
+			
+			// Send the user's details to the backend
+			const response = await axios.post('http://[::1]:8080/users', { name:username,email:useremail});
+		
+			console.log("response from send user to backend ID",response.data.id)
+			updateUserId(response.data.id)
+			// return response.data.id;
+		}catch (error) {
+			console.log("Error getting user details: ", error);
+		}
 	}
+	
 	return (
 		<>
-			{/*<div>Click sign in </div>*/}
-		<button onClick={handleSignin} >Sign in</button>
+			<button onClick={handleSignIn}>Sign in</button>
 		</>
-	)
+	);
 };
+
+
 const SignOutButton = () => {
 	const {instance} = useMsal();
 	const handleSignOut=()=>{
@@ -37,7 +68,6 @@ const SignOutButton = () => {
 	useEffect(() => {
 		const currAccount = instance.getActiveAccount();
 		if(currAccount) {
-			console.log(currAccount)
 			setName(currAccount.name)
 		}
 	}, [instance]);
